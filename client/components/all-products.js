fcb40b919bed4730b20b22e577e85bb8b46e5fd5
julie lam <jlam2@oberlin.edu>
2021-01-13 11:13:15 -0500

    Allproducts (#11)
    * big changes, really important
    
    * testing hello
    
    * fixed importing bug for All Products
    
    * WIP: AllProducts component & reducer, not working
    
    * rendering all products on allproducts page
    
    Co-authored-by: joetoenails <joe.d.tonelli@gmail.com>

---
+++ client/components/all-products.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchProducts} from '../store/products'
+
+/**
+ * COMPONENT
+ */
+export class AllProducts extends React.Component {
+  constructor() {
+    super()
+  }
+  componentDidMount() {
+    this.props.loadProducts()
+  }
+
+  render() {
+    const firstProd = this.props.products[1] || {}
+    return (
+      <div>
+        <h2>All Art</h2>
+        <div>
+          {this.props.products.map(product => <h4>{product.title}</h4>)}
+        </div>
+      </div>
+    )
+  }
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    products: state.products
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    loadProducts: () => dispatch(fetchProducts())
+  }
+}
+
+const AllProductsConnected = connect(mapState, mapDispatch)(AllProducts)
+export default AllProductsConnected

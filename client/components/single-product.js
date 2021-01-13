526b82fc0d501d2519ddb83b1a21e4ed5cb3057b
Anya Dunaif <anya.dunaif@gmail.com>
2021-01-13 17:12:55 -0500

    working on adding material ui card in single view

--- client/components/single-product.js
+++ client/components/single-product.js
@@ -1,6 +1,54 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchProduct} from '../store/singleProduct'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 50
+  },
+  media: {
+    height: 10
+  }
+})
+
+//SAMPLE IMAGES - DELETE LATER
+
+const artImageDefault =
+  'https://upload.wikimedia.org/wikipedia/commons/7/7a/Campbell%27Soup_%281965%29_Andy_Warhol_%281928-1967%29_%2849982308446%29.jpg'
+
+const artistImageDefault =
+  'https://upload.wikimedia.org/wikipedia/commons/2/2b/Andy_Warhol_by_Jack_Mitchell.jpg'
+
+// function SingleProductCard() {
+//   const classes = useStyles
+//   return (
+//     <Card className={classes.root}>
+//       <CardActionArea>
+//         <CardMedia
+//           className={classes.media}
+//           img={artImageDefault}
+//           title="Artwork"
+//         />
+//         <CardContent>
+//           <Typography variant="h6" component="h3">
+//             Artist
+//           </Typography>
+//           <Typography variant="body2" color="textSecondary" component="p">
+//             Info about artist here
+//           </Typography>
+//         </CardContent>
+//       </CardActionArea>
+//     </Card>
+//   )
+// }
 
 /**
  * COMPONENT
@@ -9,6 +57,9 @@ import {fetchProduct} from '../store/singleProduct'
 export class SingleProduct extends React.Component {
   constructor() {
     super()
+    // this.state = {
+    //   image: artImageDefault
+    // }
   }
   componentDidMount() {
     try {
@@ -20,23 +71,39 @@ export class SingleProduct extends React.Component {
 
   render() {
     const product = this.props.product || {}
-    // console.log('PRODUCT:', product)
-    // console.log('PROPS:', this.props)
-    console.log('ARTIST', product.artist)
     let artist = 'Unknown'
+    const artistImage = artistImageDefault
+    const artImage = artImageDefault
     if (product.artist) artist = product.artist.name
+    const classes = useStyles
+
     return (
       <div>
-        <h2>Title: </h2>
+        {/* <h2>Title: </h2>
         <h2>{product.title}</h2>
         <h3>Artist</h3>
-        <h3>ARTIST IMAGE</h3>
+        <img src={artistImage} />
         <h3>{artist}</h3>
         <h3>Genre</h3>
         <h3>{product.genre}</h3>
         <h3>Medium</h3>
         <h3>{product.medium}</h3>
-        <h3>IMAGE PLACEHOLDER</h3>
+        <img src={artImage} /> */}
+        <Card>
+          <CardActionArea>
+            <CardContent>
+              <CardMedia>
+                <img src={artImage} />
+              </CardMedia>
+              <Typography variant="h6" component="h3">
+                Artist
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Info about artist here
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </div>
     )
   }

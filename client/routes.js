3b1762041f3b9b8e98f213515e5f7e25b080ebd1
joetoenails <joe.d.tonelli@gmail.com>
2021-01-13 19:15:25 -0500

    do nothing with this commit

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,13 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, AllProductsConnected} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  AllProductsConnected,
+  SingleProductConnected
+} from './components'
 import {me} from './store'
 
 /**

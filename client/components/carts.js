613c841d9bdced199d3f815c90fc45f7ff0f0f75
joetoenails <joe.d.tonelli@gmail.com>
2021-01-15 01:03:46 -0500

    fixed empty cart bug

--- client/components/carts.js
+++ client/components/carts.js
@@ -17,30 +17,39 @@ export class AllCarts extends React.Component {
         return cart.active === true
       })[0]
     }
-    return (
-      <div>
-        <h2>My Active Cart</h2>
-        {this.props.user.carts &&
-          activeCart.Products.map(product => {
-            return (
-              <div key={product.id}>
-                <h3>{product.title}</h3>
-                <h4>Price:{product.price}</h4>
-                <p>Quantity:{product.CartProduct.quantity}</p>
-              </div>
-            )
-          })}
-        <small>...........................................</small>
-        <h4>
-          Subtotal:
-          {this.props.user.carts
-            ? activeCart.Products.reduce((accum, singleProduct) => {
-                return accum + singleProduct.price * 1 //needs to be quantity
-              }, 0)
-            : ' '}
-        </h4>
-      </div>
-    )
+
+    if (activeCart === '' || !activeCart) {
+      return (
+        <div>
+          <h3>Your cart is empty right now! Go shopping!</h3>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>My Active Cart</h2>
+          {this.props.user.carts &&
+            activeCart.Products.map(product => {
+              return (
+                <div key={product.id}>
+                  <h3>{product.title}</h3>
+                  <h4>Price:{product.price}</h4>
+                  <p>Quantity:{product.CartProduct.quantity}</p>
+                </div>
+              )
+            })}
+          <small>...........................................</small>
+          <h4>
+            Subtotal:
+            {this.props.user.carts
+              ? activeCart.Products.reduce((accum, singleProduct) => {
+                  return accum + singleProduct.price * 1 //needs to be quantity
+                }, 0)
+              : ' '}
+          </h4>
+        </div>
+      )
+    }
   }
 }
 

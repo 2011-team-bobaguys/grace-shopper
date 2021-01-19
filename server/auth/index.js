33db1481bd9a3898d798bda46f49a4580939e388
joetoenails <joe.d.tonelli@gmail.com>
2021-01-18 19:56:03 -0500

    new users sign up bug fixed

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const User = require('../db/models/user')
+const {User, Cart} = require('../db/models/')
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
@@ -22,6 +22,8 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
+    const newCart = await Cart.create()
+    await user.addCart(newCart)
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
